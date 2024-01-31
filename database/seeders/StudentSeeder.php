a2cd97004762229ba263040ea46a0bc9dff5742b
ShafaKhan <shafakhan2018@gmail.com>
2024-01-31 17:36:54 +0600

    student permission

--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
 
 class StudentSeeder extends Seeder
 {
@@ -13,11 +14,12 @@ class StudentSeeder extends Seeder
      */
     public function run(): void
     {
+        $faker = Faker::create();
         DB::table('students')->truncate();
         for ($i = 1; $i <= 10000; $i++) {
             DB::table('students')->insert([
                 'roll' => $i,
-                'name' => 'Student ' . $i,
+                'name' => $faker->name,
                 'registration' => 'REG-' . $i,
                 'created_at' => now(),
                 'updated_at' => now(),
