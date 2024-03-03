797f1a3a2381b29d064d689dcb9f4f33c2d24fb2
ShafaKhan <shafakhan2018@gmail.com>
2024-03-03 23:42:09 +0600

    dscddsd

--- resources/views/livewire/unlimited-manue-and-sub.blade.php
+++ resources/views/livewire/unlimited-manue-and-sub.blade.php
@@ -159,9 +159,16 @@
                                     @php
                                         function displayMenu($manues, $parentId, $level = 0)
                                         {
-                                            $menus = $manues->where('has_menu', $parentId)->where('status', 1)->sortBy('priority');
+                                            $menus = $manues
+                                                ->where('has_menu', $parentId)
+                                                ->where('status', 1)
+                                                ->sortBy('priority');
                                             foreach ($menus as $menu) {
-                                                echo '<li style="padding-left: ' . ($level * 20) . 'px;"> -> '. $menu->title . '</li>';
+                                                echo '<li style="padding-left: ' .
+                                                    $level * 20 .
+                                                    'px;"> -> ' .
+                                                    $menu->title .
+                                                    '</li>';
                                                 // Recursive call to display submenus
                                                 displayMenu($manues, $menu->id, $level + 1);
                                             }
@@ -189,3 +196,33 @@
         </div>
     </section>
 </div>
+{{-- <form>
+    <div class="form-group">
+        <label for="exampleFormControlInput1">Email address</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+    </div>
+    <div class="form-group">
+        <label for="exampleFormControlSelect1">Example select</label>
+        <select class="form-control" id="exampleFormControlSelect1">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+        </select>
+    </div>
+    <div class="form-group">
+        <label for="exampleFormControlSelect2">Example multiple select</label>
+        <select multiple class="form-control" id="exampleFormControlSelect2">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+        </select>
+    </div>
+    <div class="form-group">
+        <label for="exampleFormControlTextarea1">Example textarea</label>
+        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    </div>
+</form> --}}
